23ee418d6f3a02e9d17ee64206b50e8a0133e7e6
GeraGarSa <welcome_to_pmz@hotmail.com>
2022-03-31 17:33:51 -0600

    Dando formato

--- demo-granarium/src/App.js
+++ demo-granarium/src/App.js
@@ -5,24 +5,64 @@ function App() {
     <div className="container-can">
       <h1 className="text-center">Hola Mundo</h1>
       <div style={myStyles.boxStyles}>
-    	  <div style={{width:'200px', height:'40px', background:'white', alignSelf: 'center', border: '3px solid orange'}}><p className="text-center">Caja 1</p></div>
-        <div style={{width:'200px', height:'40px', background:'white', alignSelf: 'center', border: '3px solid orange'}}><p className="text-center">Caja 2</p></div>
-        <div style={{width:'200px', height:'40px', background:'white', alignSelf: 'center', border: '3px solid orange'}}><p className="text-center">Caja 3</p></div>
-        <div style={{width:'200px', height:'40px', background:'white', alignSelf: 'center', border: '3px solid orange'}}><p className="text-center">Caja 4</p></div>
+        <div
+          style={{
+            width: "200px",
+            height: "40px",
+            background: "white",
+            alignSelf: "center",
+            border: "3px solid orange",
+          }}
+        >
+          <p className="text-center">Caja 1</p>
+        </div>
+        <div
+          style={{
+            width: "200px",
+            height: "40px",
+            background: "white",
+            alignSelf: "center",
+            border: "3px solid orange",
+          }}
+        >
+          <p className="text-center">Caja 2</p>
+        </div>
+        <div
+          style={{
+            width: "200px",
+            height: "40px",
+            background: "white",
+            alignSelf: "center",
+            border: "3px solid orange",
+          }}
+        >
+          <p className="text-center">Caja 3</p>
+        </div>
+        <div
+          style={{
+            width: "200px",
+            height: "40px",
+            background: "white",
+            alignSelf: "center",
+            border: "3px solid orange",
+          }}
+        >
+          <p className="text-center">Caja 4</p>
+        </div>
       </div>
-    </div> 
+    </div>
   );
 }
 
 export default App;
 
-let myStyles = { 
+let myStyles = {
   boxStyles: {
-    width: '100%',
-    height: '100px',
-    border: '5px solid yellow',
-    background: '#672DA4',
-    display: 'flex', 
-    justifyContent: 'space-around',
-  }
-}
+    width: "100%",
+    height: "100px",
+    border: "5px solid yellow",
+    background: "#672DA4",
+    display: "flex",
+    justifyContent: "space-around",
+  },
+};
